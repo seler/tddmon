8ec78c55503166d09f0e119dc878045d079aa8e3
jstolarski <devnull@localhost>
2015-02-16 18:16:49 +0100

    Bump version: 0.1.0 → 0.1.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.rst') as file:
 
 setup(
     name="tddmon",
-    version="0.1.0",
+    version="0.1.1",
     description='TDD flow monitor.',
     long_description=long_description,
     url="http://kidosoft.pl",
